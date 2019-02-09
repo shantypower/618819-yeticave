93aca3c3fe76f7af2b02ab8240a03fbc34ea72d1
shantypower <shantypower4891@yandex.ru>
2019-02-10 01:05:24 +0900

    формирует файл sql для создания БД

--- schema.sql
+++ schema.sql
@@ -4,38 +4,41 @@ DEFAULT COLLATE utf8_general_ci;
 USE yeticave;
 CREATE TABLE caregories (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  cat_name CHAR(30) UNIQUE
+  cat_name CHAR(50) NOT NULL UNIQUE
 );
-CREATE TABLE lot (
+CREATE TABLE lots (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  date_add DATETIME,
-  descr CHAR(200),
-  img_src CHAR(300),
-  start_price INT,
-  date_end DATETIME,
-  price_step int,
+  date_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  lot_name CHAR(150) NOT NULL,
+  descr CHAR(200) NOT NULL,
+  img_src TEXT NOT NULL,
+  start_price INT NOT NULL,
+  date_end TIMESTAMP NOT NULL,
+  price_step INT NOT NULL,
   author_id INT UNIQUE,
   vinner_id INT UNIQUE,
-  cat_id INT
+  cat_id INT NOT NULL
 );
-CREATE TABLE lot_rate (
+CREATE TABLE lot_rates (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  date_add DATETIME,
-  rate INT,
+  date_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  rate INT NOT NULL,
   user_id INT UNIQUE,
   lot_id INT UNIQUE
 );
-CREATE TABLE user (
+CREATE TABLE users (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  reg_date DATETIME,
+  reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   email CHAR(128) NOT NULL UNIQUE,
-  user_name CHAR(300) NOT NULL UNIQUE,
+  user_name CHAR(200) NOT NULL UNIQUE,
   user_pass CHAR(200) NOT NULL,
-  avatar_src CHAR(300),
+  avatar_src TEXT,
   contacts CHAR,
   lot_id INT UNIQUE,
   rate_id INT
 );
+CREATE INDEX lot_n ON lots(lot_name);
+CREATE INDEX user_n ON users(user_name);
 
 
 
