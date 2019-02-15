95a0005e5a94a673b726782be6c61133e9b4c81d
shantypower <shantypower4891@yandex.ru>
2019-02-15 11:43:37 +0900

    убирает ранее подготовленный массив данных и корректирует логику вывода данных после обработки запросов к БД

--- index.php
+++ index.php
@@ -3,7 +3,9 @@ date_default_timezone_set('Asia/Chita');
 require_once('functions.php');
 require_once('data.php');
 
-$link = mysqli_connect($db['host'], $db['user'], $db['password'], $db['database']);
+$link = mysqli_init();
+mysqli_options($link, MYSQLI_OPT_INT_AND_FLOAT_NATIVE, 1);
+mysqli_real_connect($link, $db['host'], $db['user'], $db['password'], $db['database']);
 mysqli_set_charset($link, "utf8");
 
 $categories = [];
@@ -19,35 +21,32 @@ if (!$link) {
     $result = mysqli_query($link, $sql);
     if ($result) {
         $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
- /*        $page_content = include_template('index.php', [
-            'categories' => $categories
-        ]); */
     } else {
         $error = mysqli_error($link);
         $page_content = include_template('error.php', ['error' => $error]);
     }
     // запрос на показ девяти самых последних лотов
     $sql = 'SELECT l.lot_name, l.start_price, l.img_src, MAX(lr.rate), c.cat_name
-    FROM lots l
-    JOIN categories c
-      ON l.cat_id = c.id
-    JOIN lot_rates lr
-      ON l.id = lr.lot_id
-   WHERE l.date_end > CURRENT_DATE()
-   GROUP BY lr.lot_id
-   ORDER BY l.date_add
-    DESC LIMIT 6;';
+              FROM lots l
+              JOIN categories c
+                ON l.cat_id = c.id
+              JOIN lot_rates lr
+                ON l.id = lr.lot_id
+             WHERE l.date_end > CURRENT_DATE()
+          GROUP BY lr.lot_id
+          ORDER BY l.date_add
+              DESC LIMIT 6;';
     $result = mysqli_query($link, $sql);
     if ($result) {
         $adverts = mysqli_fetch_all($result, MYSQLI_ASSOC);
-        $page_content = include_template('index.php', [
-            'categories' => $categories,
-            'adverts' => $adverts
-        ]);
     } else {
         $error = mysqli_error($link);
         $page_content = include_template('error.php', ['error' => $error]);
     }
+    $page_content = include_template('index.php', [
+        'categories' => $categories,
+        'adverts' => $adverts
+    ]);
 }
 
 $layout_content = include_template('layout.php', [
