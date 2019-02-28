7b5bfcf16a38dc812b703bb28e15231f0e24d70e
shantypower <shantypower4891@yandex.ru>
2019-02-28 14:04:03 +0900

    добавляет блок ставки

--- templates/lot.php
+++ templates/lot.php
@@ -23,6 +23,17 @@
                     </div>
                 </div>
             </div>
+            <form class="lot-item__form" action="add-rate.php" method="post">
+            <?php $classname = isset($errors['lot-name']) ? "form__item--invalid" : "";
+            $value = isset($rate['rate']) ? $rate['rate'] : "";
+            $error = isset($errors['rate']) ? $errors['rate'] : "";?>
+                <p class="lot-item__form-item <?=$classname;?>">
+                    <label for="cost">Ваша ставка</label>
+                    <input id="cost" type="text" name="cost" placeholder="<?=text_clean($lot['MAX(lr.rate)'] + $lot['start_price'] + $lot['price_step'])?>">
+                    <span class="form__error">Введите наименование лота</span>
+                </p>
+                <button type="submit" class="button">Сделать ставку</button>
+            </form>
             <?php endif; ?>
         </div>
     </div>
