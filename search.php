2adbd661d3ce188f2acf5eb3c477ea68671e6746
shantypower <shantypower4891@yandex.ru>
2019-03-05 11:00:03 +0900

    проверяет вариант отправки пустого запроса

--- search.php
+++ search.php
@@ -7,9 +7,10 @@ require_once('core/functions.php');
 $categories = getAllCategories($link);
 $page_content = '';
 $lots = [];
+$search = '';
 $top_menu = include_template('menu.php', ['menu' => $categories]);
-$search = $_GET['search'] ?? '';
-if ($search) {
+$search = trim($_GET['search'] ?? '');
+if ($search !== '') {
     $lots = siteSearch($search, $link);
     if (count($lots) > 0) {
         $page_content = include_template('search.php', [
@@ -20,9 +21,10 @@ if ($search) {
         print(showContent($categories, $page_content, $user_data, $search, 'Результаты поиска'));
         return;
     }
-    $page_content = include_template('error.php', [
-        'top_menu' => $top_menu,
-        'error' => 'Ничего не найдено по вашему запросу'
-    ]);
-    print(showContent($categories, $page_content, $user_data, $search, 'Ничего не найдено'));
 }
+$page_content = include_template('error.php', [
+    'top_menu' => $top_menu,
+    'error' => 'Ничего не найдено по вашему запросу'
+]);
+print(showContent($categories, $page_content, $user_data, $search, 'Ничего не найдено'));
+    return;
